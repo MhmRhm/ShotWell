b2f1abd9249085c35e570073b696b30ccd856b1e
Mohammad Rahimi <rahimi.mhmmd@gmail.com>
2024-05-12 20:58:05 +0800

    Add people detector and remove infrared and depth

--- src/capture/main.cpp
+++ src/capture/main.cpp
@@ -1,12 +1,12 @@
 #include <librealsense2/rs.hpp>
+#include <opencv2/objdetect.hpp>
 #include <opencv2/opencv.hpp>
 
 int main(int argc, char *argv[]) try {
   using namespace std;
 
   rs2::config config{};
-  config.enable_stream(RS2_STREAM_DEPTH, RS2_FORMAT_Z16, 30);
-  config.enable_stream(RS2_STREAM_INFRARED, RS2_FORMAT_Y8, 30);
+  config.enable_stream(RS2_STREAM_COLOR, RS2_FORMAT_BGR8, 30);
 
   rs2::pipeline p;
   p.start(config);
@@ -15,24 +15,21 @@ int main(int argc, char *argv[]) try {
 
   while (true) {
     rs2::frameset frames = p.wait_for_frames();
-    rs2::depth_frame depth_frame = frames.get_depth_frame();
-    rs2::frame infrared_frame = frames.get_infrared_frame();
+    rs2::video_frame color_frame = frames.get_color_frame();
+    cv::Mat color_image(
+        cv::Size(color_frame.get_width(), color_frame.get_height()), CV_8UC3,
+        (void *)color_frame.get_data(), cv::Mat::AUTO_STEP);
 
-    auto width = depth_frame.get_width();
-    auto height = depth_frame.get_height();
-    float dist_to_center = depth_frame.get_distance(width / 2, height / 2);
+    auto hog{cv::HOGDescriptor{}};
+    std::vector<cv::Rect> foundLocations{};
+    hog.setSVMDetector(cv::HOGDescriptor::getDefaultPeopleDetector());
+    hog.detectMultiScale(color_image, foundLocations);
 
-    std::cout << "The camera is facing an object " << dist_to_center
-              << " meters away \r";
+    for (auto &&rect : foundLocations) {
+      cv::rectangle(color_image, rect, {255, 255, 255});
+    }
 
-    cv::Mat infrared_image(cv::Size(width, height), CV_8U,
-                           (void *)infrared_frame.get_data(),
-                           cv::Mat::AUTO_STEP);
-
-    cv::line(infrared_image, {0, 0}, {width, height}, {255, 255, 255});
-    cv::line(infrared_image, {width, 0}, {0, height}, {255, 255, 255});
-
-    cv::imshow("Display Image", infrared_image);
+    cv::imshow("Display Image", color_image);
 
     char c = cv::waitKey(25);
     if (c == 'q')
